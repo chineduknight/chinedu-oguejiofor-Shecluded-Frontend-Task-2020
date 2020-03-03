1afb18bf06fa7cd019d2fbb8cc86a2cfcd7cff5b
Chinedu Oguejiofor <=>
2020-03-03 14:32:58 +0100

    Updates

---
+++ precache-manifest.2d37a0cd62abe4e804cee63a3ceac669.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ffc6e7811b515063e9f4a67d6ab06f",
+    "url": "/chinedu-oguejiofor-Shecluded-Frontend-Task-2020/index.html"
+  },
+  {
+    "revision": "b5b8e80bf3611a5618f7",
+    "url": "/chinedu-oguejiofor-Shecluded-Frontend-Task-2020/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "53a8cbe4be1ac4890cc2",
+    "url": "/chinedu-oguejiofor-Shecluded-Frontend-Task-2020/static/css/main.c1665bb9.chunk.css"
+  },
+  {
+    "revision": "b5b8e80bf3611a5618f7",
+    "url": "/chinedu-oguejiofor-Shecluded-Frontend-Task-2020/static/js/2.bcd102aa.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/chinedu-oguejiofor-Shecluded-Frontend-Task-2020/static/js/2.bcd102aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a8cbe4be1ac4890cc2",
+    "url": "/chinedu-oguejiofor-Shecluded-Frontend-Task-2020/static/js/main.ba6d2df0.chunk.js"
+  },
+  {
+    "revision": "e323b15636f462498cf2",
+    "url": "/chinedu-oguejiofor-Shecluded-Frontend-Task-2020/static/js/runtime-main.3e96dbc7.js"
+  },
+  {
+    "revision": "3bc7154c7e206c4b3719581fe1e6ad35",
+    "url": "/chinedu-oguejiofor-Shecluded-Frontend-Task-2020/static/media/showcase.3bc7154c.jpg"
+  }
+]);
