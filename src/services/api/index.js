860211f46283a58b4e8c034677c66742f71d4fe0
Chinedu Oguejiofor <=>
2020-03-03 23:00:05 +0100

    fixed untracked files

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,17 +2,24 @@ import axios from 'axios';
 
 const CATS_API = process.env.REACT_APP_CATS_API;
 const LOTR_API = process.env.REACT_APP_LOTR_API;
+const token = process.env.REACT_APP_LOTR_TOKEN;
+const catsKey = process.env.REACT_APP_CATS_KEY;
 
 const catsAPI = axios.create({
   baseURL: CATS_API,
   timeout: 30000
 });
 
+catsAPI.defaults.headers.common = {
+  'x-api-key': catsKey
+};
+
 const lotrAPI = axios.create({
   baseURL: LOTR_API,
   timeout: 30000
 });
+
 lotrAPI.defaults.headers.common = {
-  Authorization: `bearer LNp14nSl9dUIXi7vPi0y`
+  Authorization: `bearer ${token}`
 };
 export { lotrAPI, catsAPI };
